40e05e9318cf3e979de836943a78b2b571ac62ee
zhangyanyan <zhangyanyan3@jd.com>
2018-02-06 16:05:20 +0800

    demo09

--- app/hello.js
+++ app/hello.js
@@ -5,18 +5,23 @@ class Hello extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            liked: false
+            liked: false,
+            value: 'Hello!'
         };
         this.changeState = this.changeState.bind(this);
         this.handleClick = this.handleClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
     render(){
         var text = this.state.liked ? 'like' : 'haven\'t liked';
+        var value = this.state.value;
         return(
             <div>
                 <div onClick={this.changeState}>{this.props.name} {text} world!</div>
                 <input type="text" ref="myTextInput"/>
                 <input type="button" value="Focus the text input" onClick={this.handleClick.bind(this)} />
+                <input type="text" value={value} onChange={this.handleChange}/>
+                <p>{value}</p>
             </div>
         )
     }
@@ -25,6 +30,9 @@ class Hello extends Component{
     }
     changeState(){
         this.setState({liked: !this.state.liked});
+    }
+    handleChange(){
+        
     }
 }
 export default Hello;
